85494196d34b134a4ddc354e912a979bf52d5406
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-06-15 11:24:44 +0200

    adds primitives, arrays, types, interfaces tasks

---
+++ src/01-primitives.ts
@@ -0,0 +1,16 @@
+let age: number = 5;
+const name: string = 'mango'
+let value: any = '10'
+value = 14
+
+let id: string | number | boolean = 5
+
+id = 19;
+id = 'qwerty'
+id = true
+
+
+
+console.log(age, name, value, id);
+
+export {}
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+// import './01-primitives'
+// import './02-arrays'
+// import './03-types'
+import './04-interfaces'
+
+// export {}
