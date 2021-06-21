c4c0ad5680bc84432d6ca02dfece43d8e6462955
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-06-21 17:34:14 +0200

    adds extends

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import './01-generics-fns'
-// import './02-arrays'
+// import './01-generics-fns'
+import './02-extends'
 // import './03-types'
 // import './04-interfaces'
 // import './05-enums'
