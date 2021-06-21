245af8340447cd7ad204e72266f7792c875f071e
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-06-21 22:32:42 +0200

    adds classes

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 // import './01-generics-fns'
 // import './02-extends'
 // import './03-interfaces'
-import './04-examples'
-// import './05-enums'
+// import './04-examples'
+import './05-classes';
 // import './06-functions'
 // import './07-classes'
 
-
-export {}
+export {};
