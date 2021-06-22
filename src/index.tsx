e04e3f905847640fc2bcf53b0d762a86c05af8aa
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-06-22 21:30:05 +0200

    adds TS for React

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
-// import './01-generics-fns'
-// import './02-extends'
-// import './03-interfaces'
-// import './04-examples'
-import './05-classes';
-// import './06-functions'
-// import './07-classes'
+import * as React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
+import './styles.css';
 
-export {};
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#root'),
+);
